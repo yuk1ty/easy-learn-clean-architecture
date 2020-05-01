5d4fe516a8255aff75dfd502629f8c501b1d13a6
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-05-01 23:22:50 +0900

    Add onmemory example

--- iamclean/infra/repository/user/repository.go
+++ iamclean/infra/repository/user/repository.go
@@ -4,10 +4,12 @@ import (
 	mysql "github.com/yuk1ty/easy-learn-clean-architecture/iamclean/infra/mysql/user"
 	entity "github.com/yuk1ty/easy-learn-clean-architecture/iamclean/model/entity/user"
 	repository "github.com/yuk1ty/easy-learn-clean-architecture/iamclean/model/repository/user"
+	vo "github.com/yuk1ty/easy-learn-clean-architecture/iamclean/model/vo/user"
 )
 
 type UserRepositoryImpl struct {
 	dao mysql.UserDao
+	database map[vo.UserId]entity.User // Map 連想配列
 }
 
 func NewUserRepository(dao mysql.UserDao) repository.UserRepository {
